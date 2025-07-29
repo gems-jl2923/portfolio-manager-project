dd9a01940e1642dd5ea8938e6fde8eb809e6ba5b
林润锋 <linrunfeng@linrunfengdeMacBook-Pro.local>
2025-07-29 16:26:02 +0800

    some small change for local test, if these changes don't work in your branch, pls stick to yours

--- db/db.js
+++ db/db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'n3u3da!',
-    database: 'portfolio',
+    password: 'root',
+    database: 'test',
 });
 
 module.exports = pool;
