6e24e2f7490036eb48c319acc317c3154fe02aa7
Thebe14 <hah146@pitt.edu>
2025-07-30 06:40:40 +0000

    Add the pull list to the right.

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const app = express();
 const path = require('path');
 const networthRoutes = require('./routes/networth');
 const leftPanelRoutes = require('./routes/leftpanel');
+const stockRoutes = require('./routes/stock');
 
 app.use(express.static('views')); // dashboard.html 放在 views 文件夹
 app.use('/api/networth', networthRoutes); // 注册 API 路由
 app.use('/api/left', leftPanelRoutes);
-
+app.use('/api/stock', stockRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => {
